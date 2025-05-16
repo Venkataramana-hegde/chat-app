ad655961bb7c8024ee4a6cd029fb40e30dd31e41
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-05-16 22:51:14 +0530

    Add socket backend

--- server/server.js
+++ server/server.js
@@ -4,10 +4,39 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 //Create express app and http server
 const app = express();
 const server = http.createServer(app)
+
+//Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//Show online users
+export const userSocketMap = {};  //{userId: soketId}
+
+//Socket.io connection handler
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected", userId);
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+
+        //Emit online users to all connected clients
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+        socket.on("disconnect", () => {
+            console.log("User Disocnnected", userId);
+            delete userSocketMap[userId];
+            io.emit("getOnlineUsers", Object.keys(userSocketMap))
+        })
+    }
+})
 
 //Middleware setup
 app.use(express.json({limit: "4mb"}));
@@ -15,6 +44,7 @@ app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Connect to MongoDB
 await connectDB();
